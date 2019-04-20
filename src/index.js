7ca209239ff0456d3354d240f2958a58efa7f690
Анастасия <2@ii.yy>
2019-04-20 16:04:20 +0500

    Weather graph

--- src/index.js
+++ src/index.js
@@ -8,26 +8,28 @@ async function loadCurrency() {
   const response = await fetch(currencyURL);
   const xmlTest = await response.text();
   const parser = new DOMParser();
-  const currencyData = parser.parseFromString(xmlTest, "text/xml");
-  // <Cube currency="USD" rate="1.1321" />
-  const rates = currencyData.querySelectorAll("Cube[currency][rate]");
+  const meteoData = parser.parseFromString(xmlTest, "text/xml");
+  const forecasts = meteoData.querySelectorAll("FORECAST[day][month][year][hour]");
   const result = Object.create(null);
-  for (let i = 0; i < rates.length; i++) {
-    const rateTag = rates.item(i);
-    const rate = rateTag.getAttribute("rate");
-    const currency = rateTag.getAttribute("currency");
-    result[currency] = rate;
-  }
-  result["EUR"] = 1;
-  // result["RANDOM"] = 1 + Math.random();
-  return result;
-}
+  result.hour = [];
+  result.min = [];
+  result.max = [];
+  result.heat = [];
+  result.date = [];
 
-function normalizeDataByCurrency(data, currency) {
-  const result = Object.create(null);
-  const value = data[currency];
-  for (const key of Object.keys(data)) {
-    result[key] = value / data[key];
+  for (let forecastTag of forecasts) {
+    const tempTag = forecastTag.querySelector("TEMPERATURE[max][min]");
+    const min = +tempTag.getAttribute("min");
+    const max = +tempTag.getAttribute("max");
+    const heat = forecastTag.querySelector("HEAT[min]").getAttribute("min");
+    const label = forecastTag.getAttribute("hour") + ':00';
+    const date = `${forecastTag.getAttribute("day")}.${forecastTag.getAttribute("month")}.${forecastTag.getAttribute("year")}`;
+
+    result.min.push(min);
+    result.max.push(max);
+    result.heat.push(heat);
+    result.hour.push(label);
+    result.date.push(date);
   }
   return result;
 }
@@ -35,26 +37,63 @@ function normalizeDataByCurrency(data, currency) {
 const buttonBuild = document.getElementById("btn");
 const canvasCtx = document.getElementById("out").getContext("2d");
 buttonBuild.addEventListener("click", async function() {
-  const currencyData = await loadCurrency();
-  const normalData = normalizeDataByCurrency(currencyData, "RUB");
-  const keys = Object.keys(normalData).sort((k1, k2) =>
-    compare(normalData[k1], normalData[k2])
-  );
-  const plotData = keys.map(key => normalData[key]);
+  const meteoData = await loadMeteoData();
+  const date = meteoData.date;
+  const hour = meteoData.hour;
+  const temp = meteoData.min.map((el, i) => (el + meteoData.max[i]) / 2).map(x => Math.round(x));
+  const heat = meteoData.heat;
 
   const chartConfig = {
     type: "line",
 
     data: {
-      labels: keys,
+      labels: hour,
       datasets: [
         {
-          label: "Стоимость валюты в рублях",
-          backgroundColor: "rgb(255, 20, 20)",
-          borderColor: "rgb(180, 0, 0)",
-          data: plotData
+          label: "Температура",
+          backgroundColor: "rgb(255, 87, 51, 0.5)",
+          borderColor: "rgb(255, 87, 51)",
+          data: temp
+        },
+        {
+          label: "Температура по ощущениям",
+          backgroundColor: "rgb(51, 219, 255, 0.5)",
+          borderColor: "rgb(51, 219, 255)",
+          data: heat
         }
       ]
+    },
+    options: {
+      scales: {
+        yAxes: [
+          {
+            id: "temp",
+            ticks: {
+              beginAtZero: true,
+            },
+            scaleLabel: {
+              display: true,
+              labelString: 'Температура, \u00b0\u0043'
+            }
+          }
+        ]
+      },
+      tooltips: {
+        callbacks: {
+          title: function(tooltipItems, data) {
+            return date[tooltipItems[0].index] + ' ' + tooltipItems[0].xLabel;
+          },
+          label: function(tooltipItems, data) {
+            let yValue = Number(tooltipItems.yLabel);
+            if (yValue > 0) tooltipItems.yLabel = '+' + yValue;
+            return tooltipItems.yLabel + '\u00b0\u0043';
+          }
+        },
+        titleFontSize: 16,
+        backgroundColor: '#264653',
+        bodyFontSize: 14,
+        displayColors: false
+      }
     }
   };
 
